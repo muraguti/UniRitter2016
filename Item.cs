fdcc7ab4980e9d0e7973a0e4e02069da67125f0e
muraguti <laisamuraguti@gmail.com>
2016-06-11 09:04:08 -0300

    Adding order files.

---
+++ Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrdersSystem
+{
+    public class Item
+    {
+        public int Quantity { get; set; }
+        public Guid ProductId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrdersSystem
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<Item> Items { get; set; }
+
+    }
+}
